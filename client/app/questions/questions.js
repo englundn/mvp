1d42b22feb88ad167377591e3ea62161f16715b7
Nick Englund <englundn@gmail.com>
2016-10-18 16:05:13 -0700

    Added basic styling. Everything updates properly when an answer is selected.

--- client/app/questions/questions.js
+++ client/app/questions/questions.js
@@ -16,7 +16,22 @@ angular.module('sttqz.questions', [])
   });
 
   $scope.$on('checkAnswer', function(event, data) {
+    $scope.answered = true;
+    $scope.correctAnswer = data;
+    $scope.rightCapital = stateData[$scope.stateNumber].capital;
     $scope.stateNumber = Math.floor(Math.random() * 50);
     $scope.stateName = stateData[$scope.stateNumber].name;
+
+    $location.path('/').search({state: $scope.stateName});
+
+    GetImages($scope.stateName).then(function(urls) {
+      var randomImages = [];
+      for (var i = 0; i < 3; i++) {
+        randomImages.push(Math.floor(Math.random() * urls.length));
+      }
+      $scope.imageUrls = randomImages.map(function (i) {
+        return urls[i];
+      });
+    });
   });
 }); 
