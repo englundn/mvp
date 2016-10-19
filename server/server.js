8e912e3b2812b647ad827c4b27be321e5c3c86ac
Nick Englund <englundn@gmail.com>
2016-10-18 17:14:17 -0700

    Implementing username and password.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/sttqz');
+
 var app = express();
+
 
 app.use(express.static(__dirname + '/../client'));
 
---
+++ server/users/userModel.js
@@ -0,0 +1,56 @@
+var Promise = require('bluebird');
+var mongoose = require('mongoose');
+
+var UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  highscore: {
+    type: Number,
+    default: 0
+  }
+});
+
+
+var User = mongoose.model('User', UserSchema);
+
+var findUser = Promise.promisify(User.findOne, User);
+var createUser = Promise.promisify(User.create, User);
+
+module.exports = {
+  signin: function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    findUser({username: username}).then(function(user) {
+      if (!user) {
+        console.error('User does not exist.');
+        res.send();
+      } else {
+        if (user.password === password) {
+          console.log('Logged in.');
+          res.send(user);
+        }
+      }
+    });
+  },
+  signup: function (req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    findUser({username: username}).then(function(user) {
+      if (user) {
+        console.error('User already exists!');
+        res.send();
+      } else {
+        createUser({username: username, password: password});
+        res.send({username: username, password: password});
+      }
+    });
+  }
+};
